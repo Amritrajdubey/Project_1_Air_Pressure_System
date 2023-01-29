c6abd99bec4416dd134b2ef24aaa6d467c4ba105
Amritrajdubey <amrit224raj@gmail.com>
2023-01-29 13:57:58 +0530

    Updated

--- Data_dump.py
+++ Data_dump.py
@@ -20,3 +20,5 @@ if __name__ == "__main__":
     print(json_record[0])
 
     client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+
+    
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+def get_requirements():
+    pass
+
+setup(
+    name='Sensor',
+    version ='0.0.1',
+    author='Amrit Raj',
+    author_email='amrit224raj@gmail.com',
+    packages=find_packages(),
+    install_requires = get_requirements()
+)
