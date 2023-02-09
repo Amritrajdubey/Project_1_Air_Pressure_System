3b0b6dae0ac0bc5819df39e5ac2be1332b586be9
Amritrajdubey <amrit24rajgmail.com>
2023-02-09 21:52:19 +0530

    updated the value

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -8,8 +8,8 @@ class DataIngestitionConfig :
     def __init__(self,training_pipeline_config : TrainingPipelineConfig):
         self.database_name ='aps'
         self.collection_name = 'sensor'
-        self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir , 'data_ingestion' )
-        
+        self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir , 'data_ingestion')
+
 
 
 
