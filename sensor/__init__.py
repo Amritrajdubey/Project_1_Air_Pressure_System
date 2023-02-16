04b1ffb3a290a3b38bad53baf09f85d2cce7f00a
Amritrajdubey <amrit224raj@gmail.com>
2023-02-17 02:34:34 +0530

    updated

--- sensor/__init__.py
+++
@@ -1,3 +0,0 @@
-from dotenv import load_dotenv
-print('Loading environment variable from .env file')
-load_dotenv()
--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -1,17 +1,9 @@
 from dataclasses import dataclass
 
-<<<<<<< HEAD
 @dataclass
 class DataIngestionArtifact:
     pass
 
-=======
-class DataIngestitionArtifact :
-    feature_store_file_path :str
-    train_file_path :str
-    test_file_path :str
-    
->>>>>>> be249c909a356d8213942c1c19f72c3b9c719dd4
 
 class DataValidationArtifact :...
 class DataTransformationArtifact :...
