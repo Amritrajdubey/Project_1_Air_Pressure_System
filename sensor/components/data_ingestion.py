b3191cf77e8585ae546e4dbeb93b7ccdadb8ee79
Amritrajdubey <amrit224raj@gmail.com>
2023-02-18 19:22:46 +0530

    updated the code

--- sensor/components/data_ingestion.py
+++ sensor/components/data_ingestion.py
@@ -28,7 +28,7 @@ class DataIngestion:
 
             logging.info("Save data in feature store")
 
-            #replace na with Nan
+            # replacing na with Nan
             df.replace(to_replace="na",value=np.NAN,inplace=True)
 
             #Save data in feature store
@@ -43,7 +43,7 @@ class DataIngestion:
 
             logging.info("split dataset into train and test set")
             #split dataset into train and test set
-            train_df,test_df = train_test_split(df,test_size=self.data_ingestion_config.test_size,random_state=20)
+            train_df,test_df = train_test_split(df,test_size=self.data_ingestion_config.test_size,random_state=42)
             
             logging.info("create dataset directory folder if not available")
             #create dataset directory folder if not available
