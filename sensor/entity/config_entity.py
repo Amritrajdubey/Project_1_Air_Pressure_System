4cbe6b919dbf552d7f186b213165402eb32d7e03
Amritrajdubey <amrit224raj@gmail.com>
2023-02-20 00:39:24 +0530

    updated config.entity file

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -19,15 +19,15 @@ class TrainingPipelineConfig :
 class DataIngestionConfig:
 
     def __init__(self,training_pipeline_config :TrainingPipelineConfig):
-            try:
-                self.database_name="aps"
-                self.collection_name="sensor"
-                self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir , "data_ingestion")
-                self.feature_store_file_path = os.path.join(self.data_ingestion_dir,"feature_store",FILE_NAME)
-                self.train_file_path = os.path.join(self.data_ingestion_dir,"dataset",TRAIN_FILE_NAME)
-                self.test_file_path = os.path.join(self.data_ingestion_dir,"dataset",TEST_FILE_NAME)
-                self.test_size = 0.2
-            except Exception as e:
+        try:
+            self.database_name="aps"
+            self.collection_name="sensor"
+            self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir , "data_ingestion")
+            self.feature_store_file_path = os.path.join(self.data_ingestion_dir,"feature_store",FILE_NAME)
+            self.train_file_path = os.path.join(self.data_ingestion_dir,"dataset",TRAIN_FILE_NAME)
+            self.test_file_path = os.path.join(self.data_ingestion_dir,"dataset",TEST_FILE_NAME)
+            self.test_size = 0.2
+        except Exception as e:
                 raise SensorException(e,sys)
         
 
@@ -38,7 +38,19 @@ class DataIngestionConfig:
             raise SensorException(e,sys)        
 
 
-class DataValidationConfig:...
+class DataValidationConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        try:
+            self.data_validation_dir : os.path.join(training_pipeline_config.artifact_dir,"data_validation")
+            self.report_file_path = os.path.join(self.data_validation_dir,"report.yaml")
+            self.missing_threshold :float = .2
+            self.base_file_path = os.path.join('aps_failure_training_set1.csv')
+        
+        except Exception as e:
+            raise SensorException(e,sys )
+
+
+
 class DataTransformationConfig :...
 class ModelTrainerConfig :...
 class ModelEvaluationCOnfig :...
